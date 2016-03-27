ce8d85f6f97494f2d744006b10d7bd737aec93c0
wlsgur0726 <wlsgur0726@naver.com>
2016-03-28 01:09:11 +0900

    메모리할당 테스트 - 오브젝트풀 샤딩해서 테스트

--- MemAllocSpeedTest/MemAllocSpeedTest.cpp
+++ MemAllocSpeedTest/MemAllocSpeedTest.cpp
@@ -26,13 +26,13 @@
 /*** Test Option *******************************************************************************************/
 
 //// 결과값이 너무 커서 보기 힘들면 이 숫자로 나눈다.
-const double Test_Division = 100.0;
+const double Test_Division = 1000.0;
 
 //// 테스트를 수행 할 시간
 const int Test_TimeMs = 1000 * 10;
 
-//// 쓰레드 개수 (4)
-const auto Test_TheadCount = std::thread::hardware_concurrency();
+//// 쓰레드 개수
+#define Test_TheadCount 4
 
 //// Windows에만 적용. CRITICAL_SECTION 객체의 스핀카운트
 ////  - 0이면 기본값. (제일 빠르다?)
@@ -53,29 +53,34 @@ const int Test_Object_Size = 1024 * 12;
 
 //// 오브젝트풀 사용 여부 (아래 평은 Test_Use_TLS를 사용하지 않은 경우)
 ////  - Test_Alloc_Count 또는 Test_Object_Size가 크건 작건 일관성 있는 속도 유지
-////  - Windows의 경우 락에 의해 CPU 사용률이 50%를 못넘음 (스핀락X)
-////  - Windows  :  25,000 ~ 30,000 cnt/ms
-////  - Linux    :  약 15,000 cnt/ms
-#define Test_Use_ObjPool
+////  - Windows  :  단일:25,000~30,000cnt/ms,  샤딩:약97,000cnt/ms
+////  - Linux    :  단일:약15,000cnt/ms,  샤딩:약68,000cnt/ms
+#define Test_Use_ObjPool Test_TheadCount
+//#define Test_Use_ObjPool 1
 
 //// 오브젝트풀을 std::stack으로 구현
 ////  - Windows  :  직접 stack 구현하는것에 비해 10%가량 느림
 ////  - Linux    :  비슷함
 //#define Test_Use_StdStack
 
+//// 오브젝트풀을 직접 락프리 스택으로 구현
+//    - Windows  :  단일:약8,000cnt/ms,  샤딩:약200,000cnt/ms
+//    - Linux    :  단일:약8,000cnt/ms,  샤딩:약160,000cnt/ms
+//#define Test_Use_My_LockfreeStack
+
 //// 오브젝트풀을 boost::lockfree::stack으로 구현
-////  - Windows  :  약 9,200 cnt/ms
-////  - Linux    :  약 11,500 cnt/ms
+////  - Windows  :  약 9,200cnt/ms,  샤딩:약35,000cnt/ms
+////  - Linux    :  약 11,500cnt/ms,  샤딩:약41,000cnt/ms
 //#define Test_Use_Boost_LockfreeStack
 
 //// 각 OS의 고유한 락(Windows:CRITICAL_SECTION, Linux:pthread_mutex) 대신 std::mutex를 사용
-////  - Windows  :  OS락과 비슷함
-////  - Linux    :  OS락과 비슷함
+////  - Windows  :  단일:OS락과 비슷함,  샤딩:약31,000cnt/ms
+////  - Linux    :  단일:OS락과 비슷함,  샤딩:약30,000cnt/ms
 //#define Test_Use_StdMutex
 
 //// 오브젝트풀 사용 시 스핀락 사용 여부
-////  - Windows  :  9,000  ~ 15,000 cnt/ms
-////  - Linux    :  10,000 ~ 16,000 cnt/ms
+////  - Windows  :  단일:9,000~18,000cnt/ms,  샤딩:약36,000cnt/ms
+////  - Linux    :  단일:10,000~16,000cnt/ms,  샤딩:약33,000cnt/ms
 //#define Test_Use_SpinLock
 
 //// 오브젝트 풀을 쓰레드 로컬 객체로 사용 여부 (락 미사용)
@@ -95,7 +100,7 @@ const int Test_Object_Size = 1024 * 12;
 
 /*** 결과 **************************************************************************************************/
 
-//// new횟수 + delete횟수
+//// new, delete횟수
 uint64_t g_accessCount = 0;
 
 //// 오브젝트 풀 사용 시 최대로 풀링한 데이터 개수
@@ -105,19 +110,12 @@ uint64_t g_pooledCount = 0;
 
 
 
-#ifndef Test_Use_TLS
-
-#define TLS 
-
-#else // Test_Use_TLS
 
 #if defined(_MSC_VER) && _MSC_VER <= 1800
 #define TLS __declspec(thread) // VS2013 이하
 #else
 #define TLS thread_local // C++11
 #endif
-
-#endif // Test_Use_TLS
 
 struct CS
 {
@@ -176,8 +174,7 @@ struct CS
 	void lock() {
 		bool cmp;
 		while (true) {
-			if (m_lock)
-				continue;
+			if (m_lock.load(std::memory_order_acquire)) continue;
 			cmp = false;
 			if (m_lock.compare_exchange_strong(cmp, true, std::memory_order_acquire))
 				break;
@@ -203,16 +200,43 @@ public:
 	{
 	}
 #else
-#ifndef Test_Use_StdStack
+#ifdef Test_Use_StdStack
+	std::stack<T*> m_pool;
+#else
 	struct Node
 	{
 		Node* m_next = nullptr;
 		uint8_t m_data[sizeof(T)];
+		inline Node() {}
+#ifdef Test_Use_My_LockfreeStack
+		std::atomic<int>* m_popContention = nullptr;
+		void SafeWait(const std::atomic<int>* a_diff)
+		{
+			if (m_popContention != nullptr && m_popContention == a_diff) {
+				while (m_popContention->load(std::memory_order_acquire) > 0);
+				m_popContention = nullptr;
+			}
+		}
+		~Node()
+		{
+			SafeWait(m_popContention);
+		}
+#endif
 	};
+#ifdef Test_Use_My_LockfreeStack
+	std::atomic<Node*> m_head;
+	std::atomic<size_t> m_count;
+	std::atomic<int> m_popContention;
+	ObjectPool()
+	{
+		m_head = nullptr;
+		m_count = 0;
+		m_popContention = 0;
+	}
+#else
 	Node* m_head = nullptr;
 	size_t m_count = 0;
-#else
-	std::stack<T*> m_pool;
+#endif
 #endif
 
 #if !defined(Test_Use_TLS) || defined(Test_TLS_With_Lock)
@@ -258,6 +282,29 @@ public:
 			m_pool.pop();
 			Unlock();
 		}
+#elif defined(Test_Use_My_LockfreeStack)
+		++m_popContention;
+		Node* snapshot;
+		while (true) {
+			snapshot = m_head.load(std::memory_order_acquire);
+			if (snapshot == nullptr)
+				break;
+
+			Node* next = snapshot->m_next;
+			if (m_head.compare_exchange_strong(snapshot, next, std::memory_order_acq_rel))
+				break;
+		}
+		int cnt = --m_popContention;
+		if (snapshot == nullptr)
+			snapshot = new Node;
+		else {
+			//--m_count;
+			if (cnt == 0)
+				snapshot->m_popContention = nullptr;
+			else
+				snapshot->m_popContention = &m_popContention;
+		}
+		T* ret = (T*)snapshot->m_data;
 #else
 		Lock();
 		Node* head = m_head;
@@ -286,6 +333,15 @@ public:
 		Lock();
 		m_pool.push(a_data);
 		Unlock();
+#elif defined(Test_Use_My_LockfreeStack)
+		Node* node = (Node*)((uint8_t*)a_data - offsetof(Node, m_data));
+		Node* snapshot;
+		node->SafeWait(&m_popContention);
+		do {
+			snapshot = m_head.load(std::memory_order_acquire);
+			node->m_next = snapshot;
+		} while (false == m_head.compare_exchange_strong(snapshot, node, std::memory_order_acq_rel));
+		//++m_count;
 #else
 		Node* node = (Node*)((uint8_t*)a_data - offsetof(Node, m_data));
 		Lock();
@@ -317,9 +373,11 @@ public:
 		Unlock();
 #else
 		Lock();
-		while (m_head != nullptr) {
+		while (true) {
 			Node* del = m_head;
-			m_head = m_head->m_next;
+			if (del == nullptr)
+				break;
+			m_head = del->m_next;
 			delete del;
 			++cnt;
 		}
@@ -339,21 +397,43 @@ struct TestObject
 		// memset(0)이 수행된다.
 	}
 };
-TLS ObjectPool<TestObject>* t_ObjectPool = nullptr;
+
+#if Test_Use_ObjPool && !defined(Test_Use_TLS)
+ObjectPool<TestObject> t_ObjectPool[Test_Use_ObjPool];
+#elif defined(Test_Use_TLS)
+TLS ObjectPool<TestObject>* t_ObjectPool;
+#else
+ObjectPool<TestObject>* t_ObjectPool;
+#endif
+
+static std::atomic<uint32_t> g_LastNumber;
+TLS uint32_t t_shard = g_LastNumber++;
+
 
 inline void InitObjectPool() {
+#if Test_Use_ObjPool && !defined(Test_Use_TLS)
+#else
 	g_mutex.lock();
 	if (t_ObjectPool == nullptr)
 		t_ObjectPool = new ObjectPool<TestObject>;
 	g_mutex.unlock();
+#endif
 }
 inline void UnInitObjectPool() {
 	g_mutex.lock();
+#if Test_Use_ObjPool && !defined(Test_Use_TLS)
+	for (int i=0; i<Test_Use_ObjPool; ++i) {
+		size_t pooledCount = t_ObjectPool[i].ShrinkAll();
+		printf("pool[%d] : %llu\n", i, pooledCount);
+		g_pooledCount += pooledCount;
+	}
+#else
 	g_pooledCount += t_ObjectPool->ShrinkAll();
 	if (t_ObjectPool != nullptr) {
 		delete t_ObjectPool;
 		t_ObjectPool = nullptr;
 	}
+#endif
 	g_mutex.unlock();
 }
 
@@ -365,13 +445,26 @@ inline TestObject* New() {
 inline void Free(TestObject* data) {
 	delete data;
 }
-
 #else // Test_Use_ObjPool
+
 	inline TestObject* New() {
+#if Test_Use_ObjPool && !defined(Test_Use_TLS)
+		uint32_t shard = t_shard % Test_Use_ObjPool;
+		//uint32_t shard = rand() % Test_Use_ObjPool;
+		return t_ObjectPool[shard].New();
+#else
 		return t_ObjectPool->New();
+#endif
 	}
 	inline void Free(TestObject* data) {
+#if Test_Use_ObjPool && !defined(Test_Use_TLS)
+		//uint32_t shard = std::hash<void*>()(data) % Test_Use_ObjPool;
+		static_assert(997 > Test_Use_ObjPool*2, "해시키가 너무 작음");
+		uint32_t shard = ((size_t)data % 997) % Test_Use_ObjPool;
+		return t_ObjectPool[shard].Free(data);
+#else
 		t_ObjectPool->Free(data);
+#endif
 	}
 
 #endif // Test_Use_ObjPool
@@ -445,3 +538,4 @@ int main()
 
 	return 0;
 }
+
